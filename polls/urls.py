0d445d33ff9dcc3231140b99d5ba4ca64dd00745
allen.chang <allen@CentOS.microjet.com.tw>
2015-10-21 22:35:47 +0800

    Tutorial Part3

--- polls/urls.py
+++ polls/urls.py
@@ -15,15 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+
 from . import views
-
 urlpatterns = [
+    
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^polls/', include('polls.urls')),   
 
-
-    url(r'^$', views.home),
-    #url(r'^polls/$', 'polls.views.index', name='index'),
 
 ]
